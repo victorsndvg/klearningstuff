0fe813a5c9d79935cb58d0fedc0ad965eff9a437
victorsndvg <victorsv@gmail.com>
2022-06-27 12:02:41 +0200

    krew installation script

---
+++ sandbox/00_install/01_krew/kubectltrace.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+kubectl krew install trace
+
+## Alternative
+#curl -L -o kubectl-trace.tar.gz https://github.com/iovisor/kubectl-trace/releases/download/v0.1.0-rc.1/kubectl-trace_0.1.0-rc.1_linux_amd64.tar.gz
+#tar -xvf kubectl-trace.tar.gz
+#mv kubectl-trace /usr/local/bin/kubectl-trace
